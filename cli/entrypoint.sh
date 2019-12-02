f375d207ee09d16fd144f536e5cffa11f364df92
Chris Patterson <chrispat@github.com>
2019-12-02 14:44:12 -0500

    Add deprecation warning message

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -8,6 +8,8 @@ set -e
 # Respect AWS_DEFAULT_OUTPUT if specified
 [ -n "$AWS_DEFAULT_OUTPUT" ] || export AWS_DEFAULT_OUTPUT=json
 
+echo "##[warning] This actions has been deprecated in favor of https://github.com/aws-actions.  This repo has been archived and will be made private on 12/31/2019"
+
 # Capture output
 output=$( sh -c "aws $*" )
 
