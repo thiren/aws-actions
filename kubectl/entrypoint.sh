7d09080bd0405791243100efbeea14827cb27f78
Chris Patterson <chrispat@github.com>
2019-12-02 14:47:29 -0500

    Added deprecation message

--- kubectl/entrypoint.sh
+++ kubectl/entrypoint.sh
@@ -6,4 +6,6 @@ set -e
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 
+echo "##[warning] This actions has been deprecated in favor of https://github.com/Azure/k8s-actions or the run action.  This repo has been archived and will be made private on 12/31/2019"
+
 sh -c "kubectl $*"
